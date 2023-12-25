cf68f3ca88235b3eba4a041274ac3da3b5f48ca8
fathi <fathizidan52@gmail.com>
2023-12-25 16:52:46 +0200

    update

---
+++ UsersEx/request.js
@@ -0,0 +1,45 @@
+const url = 'https://catfact.ninja/fact';
+const request = require('request');
+
+const fetchUsingRequest = function () {
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            console.log('Error fetching Chuck Norris joke:', error.message);
+        } else {
+            const fact = response.body.fact;
+            console.log("Cat Fact : ", fact)
+        }
+    })
+
+}
+fetchUsingRequest();
+
+// using axios 
+const axios = require('axios');
+const secondUrl = 'https://api.chucknorris.io/jokes/random';
+
+const fetchUsingAxios = async function () {
+    try {
+        const response = await axios.get(secondUrl);
+        const data = response.data.value;
+        console.log("chucknorris joke : /n", data);
+    } catch (err) {
+        console.log(`There was an Error!${err}`);
+    }
+
+}
+fetchUsingAxios()
+
+// using node-fetch
+const fetch = require('node-fetch');
+const fetchUsingNodeFetch = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json(); // Parse JSON response
+        console.log("Cat Fact using node-fetch:\n", data.fact);
+    } catch (err) {
+        console.log(`There was an Error! ${err}`);
+    }
+
+}
+fetchUsingNodeFetch();
