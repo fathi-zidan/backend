ecca852115a1e7c740ecc8003a36ea31e1b72e5f
fathi <fathizidan52@gmail.com>
2023-12-26 12:26:18 +0200

    update

--- serverRequest/HTTPModule.js
+++ serverRequest/HTTPModule.js
@@ -1,20 +1,25 @@
 import https from 'node:https'
 const url = 'https://catfact.ninja/fact'
 
-const request = https.request(url,(response)=>{
+const request = https.request(url, (response) => {
     let data = ''
-    response.on('data',(chunk)=>{
+    response.on('data', (chunk) => {
         data += chunk.toString()
         // console.log(chunk.toString())
     })
-    response.on('end',()=>{
-        const body = JSON.parse(data);
-        console.log(body);
+    response.on('end', () => {
+        try {
+
+            const body = JSON.parse(data);
+            console.log(body);
+        } catch (err) {
+            console.error(`Error parsing json ${err}`)
+        }
 
     })
 });
 
-request.on('error',(e)=>{
+request.on('error', (e) => {
     console.log(`Error: ${e}`)
 
 })
